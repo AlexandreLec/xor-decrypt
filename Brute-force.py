79a88d35a8575b87750f474cda7770af2c99a786
DimitriLibessart <dimitri.libessart@viacesi.fr>
2020-03-30 17:58:58 +0200

    Brute-force

---
+++ Brute-force.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 16:45:16 2020
+
+@author: Dimitri
+"""
+from itertools import combinations_with_replacement
+for item in combinations_with_replacement('abcdefghijklmnopqrstuvwxyz', 6):
+    print(''.join(item))
