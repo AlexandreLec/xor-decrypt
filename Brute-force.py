2039c0ed28180125f35cec0741c847e7c843dd5f
DimitriLibessart <dimitri.libessart@viacesi.fr>
2020-03-30 18:14:06 +0200

    Update Brute-force.py

--- Brute-force.py
+++ Brute-force.py
@@ -5,5 +5,7 @@ Created on Mon Mar 30 16:45:16 2020
 @author: Dimitri
 """
 from itertools import combinations_with_replacement
+
 for item in combinations_with_replacement('abcdefghijklmnopqrstuvwxyz', 6):
-    print(''.join(item))
+     val = (''.join(item))
+     print(val)
